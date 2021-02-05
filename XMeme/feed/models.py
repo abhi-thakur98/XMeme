9a38ecb98ae51733406021480ecf9e505e7f54d5
Abhishek Thakur <iamabhishekthakur98@gmail.com>
2021-02-05 20:39:58 +0530

    Models Created

--- XMeme/feed/models.py
+++ XMeme/feed/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Owner(models.Model):
+	name = models.CharField(max_length=100)
+	num_memes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.name
+
+class Meme(models.Model):
+	owner = models.ForeignKey(Owner,on_delete=models.CASCADE)
+	caption = models.CharField(max_length=100)
+	url = models.URLField(max_length=300)
+	timestamp = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.caption
