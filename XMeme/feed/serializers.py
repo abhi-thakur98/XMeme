3ca2385bd61de64b9f9a9004dd1292d2ddef6f46
Abhishek Thakur <iamabhishekthakur98@gmail.com>
2021-02-06 12:29:48 +0530

    Added Uniqueness Check

--- XMeme/feed/serializers.py
+++ XMeme/feed/serializers.py
@@ -1,11 +1,19 @@
 from django.db.models import fields
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 from .models import *
 
 class MemeSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Meme
 		fields = '__all__'
+		validators = [
+            UniqueTogetherValidator(
+                queryset=Meme.objects.all(),
+                fields=['owner', 'caption','url'],
+				message = 'Duplicate Data'
+            )
+        ]
 
 class OwnerSerializer(serializers.ModelSerializer):
 	class Meta:
